add236b9dfb67ca4c186461bc56b79cbf0fcf0e2
khosbilegt.b <khosbilegt.b@unitel.mn>
2024-06-30 00:07:30 +0800

    initial commit

---
+++ src/test/java/dev/khosbilegt/ExampleServiceIT.java
@@ -0,0 +1,8 @@
+package dev.khosbilegt;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleServiceIT extends ExampleServiceTest {
+    // Execute the same tests but in packaged mode.
+}
