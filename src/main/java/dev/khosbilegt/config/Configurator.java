0b4e2b0ba0511398f82ddd3ebe6ffa35c698c2f4
khosbilegt.b <khosbilegt.b@unitel.mn>
2024-07-02 00:27:53 +0800

    added configurator

---
+++ src/main/java/dev/khosbilegt/config/Configurator.java
@@ -0,0 +1,18 @@
+package dev.khosbilegt.config;
+
+import io.agroal.api.AgroalDataSource;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+public class Configurator {
+    @Inject
+    AgroalDataSource ds;
+
+    @ApplicationScoped
+    public DSLContext getContext() {
+        return DSL.using(ds, SQLDialect.POSTGRES);
+    }
+}
